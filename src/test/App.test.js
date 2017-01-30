936de45bd87709f5ee3092becad4499d3b2c6c22
Iiro Kause <iiro.kause@mattersoft.fi>
2017-01-30 13:11:53 +0200

    Add common dependencies
    - added material-ui
    - include also tap plugin for react
    - include react-localisation

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../components/App';
 
-global.requestAnimationFrame = function(){};// mock for ol
+// needed for openlayers
+global.requestAnimationFrame = function(){};
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
