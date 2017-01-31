c21625583bd25754a49eaf03a062eac7e5b892f6
Iiro Kause <iiro.kause@mattersoft.fi>
2017-01-31 12:48:41 +0200

    Run UT tests on jenkins - fix building of production build

--- jenkins-npm-build.cmd
+++ jenkins-npm-build.cmd
@@ -1,7 +1,10 @@
 set PATH=C:\Program Files\nodejs;C:\Users\Administrator\AppData\Roaming\npm;%PATH%
-set CI=true
 
 call npm prune
 call npm install
+
+set CI=true
 call npm test
+set CI=
+
 call npm run build
