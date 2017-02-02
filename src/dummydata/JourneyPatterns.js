1baeb578fa0fb1b0c7e4ffec70dbba434a1ac37d
Tomi Korhonen <tomi.korhonen@mattersoft.fi>
2017-02-02 08:43:31 +0200

    Added transportMode to line

--- src/dummydata/JourneyPatterns.js
+++ src/dummydata/JourneyPatterns.js
@@ -3,19 +3,19 @@ const DummyJourneyPatterns = [
     'id': '234862983467',
     'variant': null,
     'directionOfLine': {'direction': '1', 'description': 'Rautatientori-Käpylä-Veräjälaakso'},
-    'line': {'designation': '65'}
+    'line': {'designation': '65', 'transportMode': 'BUS'}
   },
   {
     'id': '234862983468',
     'variant': null,
     'directionOfLine': {'direction': '2', 'description': 'Veräjälaakso-Käpylä-Rautatientori'},
-    'line': {'designation': '65'}
+    'line': {'designation': '65', 'transportMode': 'BUS'}
   },
   {
     'id': '234862983469',
     'variant': '5',
     'directionOfLine': {'direction': '1', 'description': 'Rautatientori-Käpylä-Veräjälaakso'},
-    'line': {'designation': '65'}
+    'line': {'designation': '65', 'transportMode': 'BUS'}
   }
 ];
 
