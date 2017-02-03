0a4498d0300d6d8d9273f99612291865999c815d
Tomi Korhonen <tomi.korhonen@mattersoft.fi>
2017-02-03 12:09:34 +0200

    Added more mock data to vehicle panel

---
+++ src/dummydata/VehicleBasicData.js
@@ -0,0 +1,10 @@
+const DummyVehicleBasicData = 
+  {
+    'id': '93302392392320',
+    'sideNumber': '5555',
+    'vehicleClass': 'C',
+    'sideNumber': '5555',
+    'age': 5
+  };
+
+export { DummyVehicleBasicData };
---
+++ src/dummydata/VehicleOperatingDayJourneys.js
@@ -0,0 +1,40 @@
+const DummyVehicleOperatingDayJourneys = [
+  {
+    'id': '79904567865',
+    'directionOfLine': {'direction': '1', 'description': 'Rautatientori-Käpylä-Veräjälaakso'},
+    'line': {'designation': '65'},
+    'plannedStartTime': '2017-02-03T07:20:00.000',
+    'plannedEndTime': '2017-02-03T07:50:00.000',
+    'actualStartTime': '2017-02-03T07:21:04.000',
+    'actualEndTime': '2017-02-03T07:49:23.000',
+  },
+  {
+    'id': '79904567866',
+    'directionOfLine': {'direction': '2', 'description': 'Veräjälaakso-Käpylä-Rautatientori'},
+    'line': {'designation': '65'},
+    'plannedStartTime': '2017-02-03T07:55:00.000',
+    'plannedEndTime': '2017-02-03T08:30:00.000',
+    'actualStartTime': '2017-02-03T07:54:01.000',
+    'actualEndTime': '2017-02-03T08:45:52.000',
+  },
+  {
+    'id': '79904567867',
+    'directionOfLine': {'direction': '1', 'description': 'Rautatientori-Käpylä-Veräjälaakso'},
+    'line': {'designation': '65'},
+    'plannedStartTime': '2017-02-03T08:40:00.000',
+    'plannedEndTime': '2017-02-03T09:10:00.000',
+    'actualStartTime': '2017-02-03T08:46:09.000',
+    'actualEndTime': '2017-02-03T09:09:23.000',
+  },
+  {
+    'id': '79904567868',
+    'directionOfLine': {'direction': '2', 'description': 'Veräjälaakso-Käpylä-Rautatientori'},
+    'line': {'designation': '65'},
+    'plannedStartTime': '2017-02-03T09:20:00.000',
+    'plannedEndTime': '2017-02-03T09:55:00.000',
+    'actualStartTime': '2017-02-03T09:21:04.000',
+    'actualEndTime': '2017-02-03T09:49:00.000',
+  }
+];
+
+export { DummyVehicleOperatingDayJourneys };
