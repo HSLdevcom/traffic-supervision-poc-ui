44a666c39c21a6fc7f9f9780101edcb4776aa7b1
Tomi Korhonen <tomi.korhonen@mattersoft.fi>
2017-03-13 14:46:35 +0200

    Added delay to dummy data

--- src/dummydata/VehicleLocationData.js
+++ src/dummydata/VehicleLocationData.js
@@ -4,92 +4,110 @@ const DummyVehicleLocationData =
       'latitude' : 60.1704485,
       'longitude' : 24.9439185,
       'timestamp': '2017-02-03T07:02:00.000',
-      'speed': 0.0
+      'speed': 0.0,
+      'delay': 0
     }, {
       'latitude' : 60.1704755,
       'longitude' : 24.9438825,
       'timestamp': '2017-02-03T07:02:01.000',
-      'speed': 5.0
+      'speed': 5.0,
+      'delay': 0
     }, {
       'latitude' : 60.1707075,
       'longitude' : 24.9444065,
       'timestamp': '2017-02-03T07:02:02.000',
-      'speed': 10.0
+      'speed': 10.0,
+      'delay': 0
     }, {
       'latitude' : 60.1706275,
       'longitude' : 24.9451845,
       'timestamp': '2017-02-03T07:02:03.000',
-      'speed': 15.0
+      'speed': 15.0,
+      'delay': 2
     }, {
       'latitude' : 60.1712075,
       'longitude' : 24.9469125,
       'timestamp': '2017-02-03T07:02:04.000',
-      'speed': 20.0
+      'speed': 20.0,
+      'delay': 3
     }, {
       'latitude' : 60.171345,
       'longitude' : 24.9472045,
       'timestamp': '2017-02-03T07:02:05.000',
-      'speed': 50.0
+      'speed': 50.0,
+      'delay': 4
     }, {
       'latitude' : 60.1714465,
       'longitude' : 24.9473695,
       'timestamp': '2017-02-03T07:02:06.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': 5
     }, {
       'latitude' : 60.1715445,
       'longitude' : 24.947485,
       'timestamp': '2017-02-03T07:02:07.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': 30
     }, {
       'latitude' : 60.1717235,
       'longitude' : 24.9475755,
       'timestamp': '2017-02-03T07:02:08.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': 61
     }, {
       'latitude' : 60.1719025,
       'longitude' : 24.9477075,
       'timestamp': '2017-02-03T07:02:10.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': 60
     }, {
       'latitude' : 60.1721345,
       'longitude' : 24.9478575,
       'timestamp': '2017-02-03T07:02:11.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': 75
     }, {
       'latitude' : 60.1727745,
       'longitude' : 24.9489335,
       'timestamp': '2017-02-03T07:02:12.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': 80
     }, {
       'latitude' : 60.1730515,
       'longitude' : 24.9489115,
       'timestamp': '2017-02-03T07:02:13.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': 120
     }, {
       'latitude' : 60.1734515,
       'longitude' : 24.9493545,
       'timestamp': '2017-02-03T07:02:14.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': 125
     }, {
       'latitude' : 60.1736385,
       'longitude' : 24.9495575,
       'timestamp': '2017-02-03T07:02:15.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': 110
     }, {
       'latitude' : 60.1738435,
       'longitude' : 24.9497985,
       'timestamp': '2017-02-03T07:02:16.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': 110
     }, {
       'latitude' : 60.1740755,
       'longitude' : 24.9500565,
       'timestamp': '2017-02-03T07:02:17.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': 110
     }, {
       'latitude' : 60.1742895,
       'longitude' : 24.9502255,
       'timestamp': '2017-02-03T07:02:18.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': 110
     }, {
       'latitude' : 60.1744955,
       'longitude' : 24.9503215,
@@ -99,12 +117,14 @@ const DummyVehicleLocationData =
       'latitude' : 60.1746295,
       'longitude' : 24.9503255,
       'timestamp': '2017-02-03T07:02:20.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': 110
     }, {
       'latitude' : 60.1750245,
       'longitude' : 24.95035,
       'timestamp': '2017-02-03T07:02:22.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': 70
     }, {
       'latitude' : 60.1750245,
       'longitude' : 24.95035,
@@ -114,77 +134,92 @@ const DummyVehicleLocationData =
       'latitude' : 60.1758155,
       'longitude' : 24.9502155,
       'timestamp': '2017-02-03T07:02:42.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': 60
     }, {
       'latitude' : 60.1768925,
       'longitude' : 24.9501385,
       'timestamp': '2017-02-03T07:02:49.000',
-      'speed': 65.0
+      'speed': 65.0,
+      'delay': 50
     }, {
       'latitude' : 60.1780155,
       'longitude' : 24.9500445,
       'timestamp': '2017-02-03T07:02:54.000',
-      'speed': 85.0
+      'speed': 85.0,
+      'delay': 40
     }, {
       'latitude' : 60.1781055,
       'longitude' : 24.9500655,
       'timestamp': '2017-02-03T07:03:00.000',
-      'speed': 105.0
+      'speed': 105.0,
+      'delay': 30
     }, {
       'latitude' : 60.1782745,
       'longitude' : 24.9502145,
       'timestamp': '2017-02-03T07:03:01.000',
-      'speed': 95.0
+      'speed': 95.0,
+      'delay': 0
     }, {
       'latitude' : 60.1784175,
       'longitude' : 24.9503085,
       'timestamp': '2017-02-03T07:03:02.000',
-      'speed': 75.0
+      'speed': 75.0,
+      'delay': -20
     }, {
       'latitude' : 60.1788485,
       'longitude' : 24.9505675,
       'timestamp': '2017-02-03T07:03:05.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -40
     }, {
       'latitude' : 60.1788485,
       'longitude' : 24.9502675,
       'timestamp': '2017-02-03T07:03:11.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -65
     }, {
       'latitude' : 60.1793065,
       'longitude' : 24.9502265,
       'timestamp': '2017-02-03T07:03:21.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -85
     }, {
       'latitude' : 60.1798635,
       'longitude' : 24.950175,
       'timestamp': '2017-02-03T07:03:31.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -180
     }, {
       'latitude' : 60.1800945,
       'longitude' : 24.9505555,
       'timestamp': '2017-02-03T07:03:41.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.180425,
       'longitude' : 24.951435,
       'timestamp': '2017-02-03T07:03:51.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1810715,
       'longitude' : 24.9532145,
       'timestamp': '2017-02-03T07:04:01.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1821545,
       'longitude' : 24.9561295,
       'timestamp': '2017-02-03T07:04:01.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1821895,
       'longitude' : 24.9562025,
       'timestamp': '2017-02-03T07:04:03.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1823485,
       'longitude' : 24.9565855,
@@ -199,232 +234,278 @@ const DummyVehicleLocationData =
       'latitude' : 60.1825435,
       'longitude' : 24.9569885,
       'timestamp': '2017-02-03T07:04:06.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1830475,
       'longitude' : 24.9580295,
       'timestamp': '2017-02-03T07:04:08.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1832065,
       'longitude' : 24.958345,
       'timestamp': '2017-02-03T07:04:13.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.183255,
       'longitude' : 24.9584145,
       'timestamp': '2017-02-03T07:04:14.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1840965,
       'longitude' : 24.9590845,
       'timestamp': '2017-02-03T07:04:15.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1845675,
       'longitude' : 24.960115,
       'timestamp': '2017-02-03T07:04:16.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1848345,
       'longitude' : 24.9604435,
       'timestamp': '2017-02-03T07:04:17.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1852355,
       'longitude' : 24.9608335,
       'timestamp': '2017-02-03T07:04:18.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1854855,
       'longitude' : 24.9610025,
       'timestamp': '2017-02-03T07:04:19.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1859065,
       'longitude' : 24.9611415,
       'timestamp': '2017-02-03T07:04:20.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1863275,
       'longitude' : 24.9612795,
       'timestamp': '2017-02-03T07:04:21.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1872415,
       'longitude' : 24.9615045,
       'timestamp': '2017-02-03T07:04:23.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1876885,
       'longitude' : 24.9617885,
       'timestamp': '2017-02-03T07:04:23.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1877055,
       'longitude' : 24.9618785,
       'timestamp': '2017-02-03T07:04:24.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1877655,
       'longitude' : 24.9622775,
       'timestamp': '2017-02-03T07:04:25.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1878265,
       'longitude' : 24.9625675,
       'timestamp': '2017-02-03T07:04:26.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1879315,
       'longitude' : 24.962935,
       'timestamp': '2017-02-03T07:04:28.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1879665,
       'longitude' : 24.963045,
       'timestamp': '2017-02-03T07:04:29.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1879665,
       'longitude' : 24.963045,
       'timestamp': '2017-02-03T07:04:30.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1880455,
       'longitude' : 24.9632045,
       'timestamp': '2017-02-03T07:04:31.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1881525,
       'longitude' : 24.9633885,
       'timestamp': '2017-02-03T07:04:32.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.1882055,
       'longitude' : 24.9644615,
       'timestamp': '2017-02-03T07:04:33.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.22964,
       'longitude' : 24.974295,
       'timestamp': '2017-02-03T07:24:15.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.229888,
       'longitude' : 24.974753,
       'timestamp': '2017-02-03T07:24:16.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.230295,
       'longitude' : 24.975524,
       'timestamp': '2017-02-03T07:24:17.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.230481,
       'longitude' : 24.975854,
       'timestamp': '2017-02-03T07:24:18.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -175
     }, {
       'latitude' : 60.230596,
       'longitude' : 24.976074,
       'timestamp': '2017-02-03T07:24:19.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -105
     }, {
       'latitude' : 60.230792,
       'longitude' : 24.976369,
       'timestamp': '2017-02-03T07:24:20.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -75
     }, {
       'latitude' : 60.23088,
       'longitude' : 24.976516,
       'timestamp': '2017-02-03T07:24:21.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -15
     }, {
       'latitude' : 60.231129,
       'longitude' : 24.97683,
       'timestamp': '2017-02-03T07:24:22.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': 15
     }, {
       'latitude' : 60.231209,
       'longitude' : 24.976922,
       'timestamp': '2017-02-03T07:24:23.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -5
     }, {
       'latitude' : 60.231414,
       'longitude' : 24.977127,
       'timestamp': '2017-02-03T07:24:25.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -15
     }, {
       'latitude' : 60.231699,
       'longitude' : 24.977406,
       'timestamp': '2017-02-03T07:24:26.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -5
     }, {
       'latitude' : 60.231699,
       'longitude' : 24.977406,
       'timestamp': '2017-02-03T07:24:27.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -15
     }, {
       'latitude' : 60.231904,
       'longitude' : 24.977629,
       'timestamp': '2017-02-03T07:24:28.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -35
     }, {
       'latitude' : 60.232385,
       'longitude' : 24.978113,
       'timestamp': '2017-02-03T07:24:29.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -45
     }, {
       'latitude' : 60.232546,
       'longitude' : 24.97828,
       'timestamp': '2017-02-03T07:24:30.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -35
     }, {
       'latitude' : 60.232715,
       'longitude' : 24.978447,
       'timestamp': '2017-02-03T07:24:31.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -25
     }, {
       'latitude' : 60.232911,
       'longitude' : 24.978616,
       'timestamp': '2017-02-03T07:24:32.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -25
     }, {
       'latitude' : 60.233126,
       'longitude' : 24.978712,
       'timestamp': '2017-02-03T07:24:33.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -25
     }, {
       'latitude' : 60.233233,
       'longitude' : 24.978752,
       'timestamp': '2017-02-03T07:24:41.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -25
     }, {
       'latitude' : 60.233341,
       'longitude' : 24.978755,
       'timestamp': '2017-02-03T07:24:51.000',
-      'speed': 55.0
+      'speed': 55.0,
+      'delay': -25
     }, {
       'latitude' : 60.233557,
       'longitude' : 24.978707,
       'timestamp': '2017-02-03T07:25:01.000',
-      'speed': 45.0
+      'speed': 45.0,
+      'delay': -25
     }, {
       'latitude' : 60.233764,
       'longitude' : 24.978605,
       'timestamp': '2017-02-03T07:25:05.000',
-      'speed': 35.0
+      'speed': 35.0,
+      'delay': -25
     }, {
       'latitude' : 60.233782,
       'longitude' : 24.978588,
       'timestamp': '2017-02-03T07:25:06.000',
-      'speed': 10.0
+      'speed': 10.0,
+      'delay': -25
     }, {
       'latitude' : 60.233936,
       'longitude' : 24.978466,
       'timestamp': '2017-02-03T07:25:09.000',
-      'speed': 2.0
+      'speed': 2.0,
+      'delay': -25
     }
 ];
 
